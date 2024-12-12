7a765c77c61839b361c14acb1f54ca22be034e74
Arpit joshi <your-arpitjoshi@gmail.com>
2024-12-12 21:40:32 +0530

    get in touch added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -11,6 +11,7 @@ import gal6 from "../../assets/product6.jpg"
 import gal7 from "../../assets/product7.jpg"
 import gal8 from "../../assets/product8.jpg"
 import gal9 from "../../assets/product9.png"
+import gal10 from "../../assets/product11.jpg"
 
 
 export default function Home() {
@@ -170,8 +171,105 @@ export default function Home() {
                             className="w-full h-64 object-cover"
                         />
                     </div>
+                    <div className="block md:hidden">
+                        <img
+                            src={gal10}
+                            alt=""
+                            className="w-full h-64 object-cover"
+                        />
+                    </div>
                 </div>
             </section>
+
+            {/* get in touch */}
+            <section className="bg-primary">
+  <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-20 lg:px-8">
+    <div className="max-w-2xl lg:max-w-4xl mx-auto text-center">
+      <h2 className="text-3xl font-extrabold text-black">Visit Our Location</h2>
+      <p className="mt-4 text-lg text-black">
+      Come explore our store and experience the finest products, handcrafted with care and tradition. We’re here to assist you in finding what you need.
+      </p>
+    </div>
+    <div className="mt-16 lg:mt-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        {/* Map Section */}
+        <div className="rounded-lg overflow-hidden">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3485.344403770432!2d79.51735077556062!3d29.12503047539949!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39a0854e46fbbcbb%3A0xe8ad2dca724e9699!2sGraphic%20Era%20Hill%20University%2C%20Haldwani%20Campus!5e0!3m2!1sen!2sin!4v1734019067737!5m2!1sen!2sin"
+            width="100%"
+            height="480"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+          ></iframe>
+        </div>
+
+        {/* Address Section */}
+        <div>
+          <div className="max-w-full mx-auto rounded-lg overflow-hidden">
+            <div className="px-6 py-4">
+              <h3 className="text-lg font-medium text-gray-900">Our Address</h3>
+              <p className="mt-1 text-gray-600">
+              Tularampur, Near Mahalaxmi Temple, opp. Middas Squar, Haldwani, Uttarakhand 263139
+              </p>
+            </div>
+            <div className="border-t border-gray-200 px-6 py-4">
+              <h3 className="text-lg font-medium text-gray-900">Hours</h3>
+              <p className="mt-1 text-gray-600">Monday - Friday: 9am - 5pm</p>
+              <p className="mt-1 text-gray-600">Saturday: 10am - 4pm</p>
+              <p className="mt-1 text-gray-600">Sunday: Closed</p>
+            </div>
+            <div className="border-t border-gray-200 px-6 py-4">
+  <h3 className="text-lg font-medium text-gray-900">Contact</h3>
+  <p className="mt-1 text-gray-600">
+    Email:{" "}
+    <a
+      href="mailto:Contact@himras.in"
+      className="text-blue-500 hover:underline"
+    >
+      Contact@himras.in
+    </a>
+  </p>
+  <p className="mt-1 text-gray-600">
+    Phone:{" "}
+    <a
+      href="tel:+918077129142"
+      className="text-blue-500 hover:underline"
+    >
+      +91 8077129142
+    </a>
+  </p>
+  <p className="mt-1 text-gray-600">
+    LinkedIn:{" "}
+    <a
+      href="https://www.linkedin.com/company/himras/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-blue-500 hover:underline"
+    >
+      Himras
+    </a>
+  </p>
+  <p className="mt-1 text-gray-600">
+    Instagram:{" "}
+    <a
+      href="https://www.instagram.com/the_himras_store"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-blue-500 hover:underline"
+    >
+      @the_himras_store
+    </a>
+  </p>
+</div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
         </div>
     );
 }
