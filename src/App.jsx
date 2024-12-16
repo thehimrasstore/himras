4311eb2d5bca56787d3db9af5a5ccd639f99b587
0-Dinesh-Chandra-Pandey <dchandrap973@gmail.com>
2024-12-16 20:27:14 +0530

    Fixed: Homepage Redirection

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
 import Shop from './components/shop/Shop';
@@ -14,19 +14,18 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
+        {/* Redirect root to /home */}
+        <Route path="/" element={<Navigate to="/home" />} />
+        
         <Route path="/home" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         {/* <Route path="/signup" element={<Signup />} /> */}
         {/* Added Dynamic Routes */}
-        <Route
-          path="/shop/product/:productId"
-          element={<ProductDetails />}
-        />
+        <Route path="/shop/product/:productId" element={<ProductDetails />} />
         <Route path="/mycart" element={<Cart />} />
         <Route path="/aboutus" element={<ComingSoon />} />
         <Route path="/blogs" element={<ComingSoon />} />
         <Route path="/contactus" element={<ComingSoon />} />
-       
       </Routes>
       <Footer />
     </Router>
